03fc56054949a53af9e5c318bf1a1b36b39d62c2
DenisPetrov <imxaort@gmail.com>
2014-10-27 16:32:10 +0400

    стили для кнопок

--- pages/users.php
+++ pages/users.php
@@ -15,3 +15,9 @@ $(document).ready(function() {
 <div class="page-header">
 	<h1>Пользователи</h1>
 </div>
+
+<div class="page-header-menu">
+	<button class="btn btn-flat btn-sm btn-labeled btn-success"><span class="btn-label icon fa fa-camera-retro"></span>Actions</button>
+	<button class="btn btn-flat btn-xs btn-labeled btn-danger"><span class="btn-label icon fa fa-camera-retro"></span>Extra small</button>
+	<button class="btn btn-flat btn-labeled btn-primary"><span class="btn-label icon fa fa-camera-retro"></span>Default</button>
+</div>
